2df02121a5a0f5fc78d3f149489abc755fb47588
sabrilassouli <sabri.lassouli@student.ehb.be>
2021-12-07 17:09:27 +0100

    half finnished functions
    they don't work yet but the basics are there

--- index.js
+++ index.js
@@ -1,22 +1,62 @@
+window.onload = () => {
+    move();
+    getForm();
+}
+let list = [];
 
-
-async function getData(){
+async function getData() {
     let url = `https://s7-team-tajar.herokuapp.com/challenges`
     let resp = await fetch(url);
     return await resp.json();
+}
 
+async function move() {
+    let data = await getData();
+    postForm();
+    //   console.log(data);
 
 }
 
+function postForm() {
+    const formData = {
+        name: document.getElementById("name").value,
+        points: document.getElementById("points").value,
+        course: document.getElementById("course").value,
+        session: document.getElementById("session").value,
+    };
 
-window.onload = ()=> {
-
-    async function move(){
-        let data = await getData();
-        console.log(data);
-    }
-
-    move();
-
+    document.getElementById('forms').addEventListener('submit', (event) => {
+        event.preventDefault();
+        fetch('https://s7-team-tajar.herokuapp.com/saveChallenges', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            }).then(response => response.json())
+            .then(data => {
+                console.log('Has been submitted!:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    });
 }
 
+
+async function getForm() {
+    await fetch(`https://s7-team-tajar.herokuapp.com/challenges`)
+        .then(response => response.json())
+        .then(data => {
+            console.log("get data from heroku");
+            console.log(data);
+            list = data.results;
+        })
+    console.log('make a foreach with the data');
+    list.forEach(element => {
+        console.log("test")
+
+    });
+
+
+}
